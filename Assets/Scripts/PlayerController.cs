ff9d714c1b0ab1525283f6d404452209985ebea7
Z6 <zeta-6po@outlook.com>
2023-03-02 19:06:35 +0100

    fix: post-merge problem
    speed problem + bad merged

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,25 +1,39 @@
 
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 500;
+    public float moveSpeed = 200;
     public Rigidbody2D rb;
     public Animator animator;
     private Vector3 velocity = new Vector3(0,0,0);
+    
     void FixedUpdate()
     {
-        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        float verticalMovement = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        float h_move = Input.GetAxis("Horizontal");
+        float v_move = Input.GetAxis("Vertical");
+        
+        float horizontalMovement = h_move * moveSpeed * Time.deltaTime;
+        float verticalMovement =  v_move * moveSpeed * Time.deltaTime;
 
-        MovePlayer(horizontalMovement, verticalMovement);
+        if (horizontalMovement != 0 && verticalMovement != 0)
+        {
+            double x = Math.Cos(90)*horizontalMovement;
+            double y = Math.Sin(90)*verticalMovement;
+            MovePlayer((float)x, (float)y);
+        }
+        else
+        {
+            MovePlayer(-horizontalMovement, verticalMovement);
+        }
         animator.SetFloat("SpeedX", rb.velocity.x);
         animator.SetFloat("SpeedY", rb.velocity.y);
     }
     
     void MovePlayer(float _horizontalMovement, float _verticvalMovement)
     {
-        Vector3 targetVelocity = new Vector2(_horizontalMovement, _verticvalMovement);
+        Vector3 targetVelocity = new Vector2(-_horizontalMovement, _verticvalMovement);
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
     }
 }
