7f2712dd00fcf11aff30f0af486e4584553bdb4b
Z6 <zeta-6po@outlook.com>
2023-03-08 15:37:21 +0100

    fix: somes things that need to be changed for the soutenance

--- Assets/Scripts/SceneController/Interior/First/IntFirstMusicStoreSceneController.cs
+++ Assets/Scripts/SceneController/Interior/First/IntFirstMusicStoreSceneController.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 //This class is responsible for handling every single event in the shop
 public class IntFirstMusicStoreSceneController : SceneController
 {
-    
+//event function triggered when touching exiting point 
+    public void OnExitScene()
+    {
+        SceneManager.LoadScene("ExtFirstScene");
+    }
 }
