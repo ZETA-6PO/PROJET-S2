a3c71863ded4bb611f4b541cd2171354459bdd3c
Z6 <zeta-6po@outlook.com>
2023-05-24 16:54:57 +0200

    feat : battle system finished

--- Assets/Scripts/ATH/ATH.cs
+++ Assets/Scripts/ATH/ATH.cs
@@ -8,9 +8,20 @@ public class ATH : MonoBehaviour
     public TMP_Text coinText;
     
     // Update is called once per frame
-    void Update()
+    public void UpdateCoin()
     {
         coinText.text = GameManager.Instance.coin.ToString();
+    }
+
+    public void EnableATH()
+    {
+        gameObject.SetActive(true);
+        UpdateCoin();
+    }
+
+    public void DisableATH()
+    {
+        gameObject.SetActive(false);
     }
 
     public void ButtonPlayerMenu()
