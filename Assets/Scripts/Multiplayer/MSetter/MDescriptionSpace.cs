9df5d0e4f6028dd4970752c60b0f8feb1d3ae8e9
Z6 <zeta-6po@outlook.com>
2023-05-27 16:28:09 +0200

    correction de bug dans le menu et correction du multiplayer fighter

--- Assets/Scripts/Multiplayer/MSetter/MDescriptionSpace.cs
+++ Assets/Scripts/Multiplayer/MSetter/MDescriptionSpace.cs
@@ -8,12 +8,8 @@ public class MDescriptionSpace : MonoBehaviour
     [SerializeField] private GameObject _content;
     [SerializeField] private GameObject _cellprefab;
     [SerializeField] private TMP_Text _itemName;
-    private MSetter _launcher;
     
-    public void Initialize(MSetter launcher)
-    {
-        _launcher = launcher;
-    }
+    
     public void Change(Item item)
     {
         _itemName.text = item.name;
@@ -23,16 +19,16 @@ public class MDescriptionSpace : MonoBehaviour
             AddCell("Damages :",a.damage.ToString());
             AddCell("Effect :",a.effect.ToString());
             AddCell("Rarity :",a.rarity.ToString());
-            AddCell("Resistance Impact :",a.ResistanceImpact.ToString());
+            AddCell("Impact :",a.ResistanceImpact.ToString());
             AddCell("Max Use :",a.MaxUse.ToString());
-            AddCell("Inspiration cost :",a.InspirationCost.ToString());
+            AddCell("Cost :",a.InspirationCost.ToString());
             
         }
         if (item is Consumable)
         {
             Consumable c = (Consumable)item;
-            AddCell("Inspiration added",c.addedInspiration.ToString());
-            AddCell("Resistance added",c.addedResistance.ToString());
+            AddCell("Inspiration :",c.addedInspiration.ToString());
+            AddCell("Resistance :",c.addedResistance.ToString());
         }
     }
 
